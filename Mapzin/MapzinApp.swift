f99e60731129b0ce252d78e9929742b969f1fa37
amir <malamud95@gmail.com>
2024-03-17 09:59:01 +0200

    Initial Commit

---
+++ Mapzin/MapzinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapzinApp.swift
+//  Mapzin
+//
+//  Created by Amir Malamud on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MapzinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
