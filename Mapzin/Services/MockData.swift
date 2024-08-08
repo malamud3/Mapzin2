dca1685ec62465197c1f59bb532dbb2807b7f203
amir <malamud95@gmail.com>
2024-08-08 09:29:27 +0300

    name fix

--- Mapzin/Services/MockData.swift
+++ Mapzin/Services/MockData.swift
@@ -18,7 +18,7 @@ struct MockData {
     static let poi2 = POI(id: UUID(), name: "Door", description: "Main conference room on the floor", category: "Meeting Room", position: position2, buildingID: UUID(), levelID: UUID())
     
     // Mock Floor data
-    static let floor = Floor(id: UUID(), name: "Ground Floor", levelIndex: 0, pois: [poi1, poi2], mapImage: "ground_floor_map.png")
+    static let floor = Floor(id: UUID(), name: "Ground Floor", levelIndex: 0, pois: [poi1, poi2])
     
     // Mock Building data
     static let building = Building(name: "Tech Hub", imageName: "tech_hub.png", coordinates: CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194), address: "123 Tech Lane, San Francisco, CA", floors: [floor], link: "https://www.techhub.com")
